25ef2956316d3a58b8514b64353cb2345939e606
ArijitGuha0179 <archi4arijit@gmail.com>
2024-09-19 17:13:05 +0530

    changes made

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -85,7 +85,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://github.com/ArijitGuha0179/MyPortfolio/blob/main/Arijit_CV.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'C++',
